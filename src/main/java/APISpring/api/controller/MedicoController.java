ab8fe853871ea217622b082dc67f585504d4b1c3
Pedro Otero <pedro@PEDROOTERO.cooxupe.com.br>
2023-02-07 10:41:12 -0300

    intial commit

--- src/main/java/APISpring/api/controller/MedicoController.java
+++ src/main/java/APISpring/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package APISpring.api.controller;
 
 import APISpring.api.medico.DadosCadastroMedico;
+import APISpring.api.medico.Medico;
+import APISpring.api.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        //System.out.println(dados);
+        repository.save(new Medico(dados));
 
     }
 }
